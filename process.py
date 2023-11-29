3964669a68c01e0f5fa7389bd197cc947d5152df
Ren Miyamoto <miyamoto@gavo.t.u-tokyo.ac.jp>
2023-11-29 16:43:16 +0900

    selectの初期値が動的に変更されるように改善

--- process.py
+++ process.py
@@ -279,32 +279,43 @@ app = Flask(__name__)
 
 @app.route("/", methods=["GET", "POST"])
 def upload_file():
+    methods = [
+            {"display": "robopitch, 窓長512サンプル", "code": "robopitch_512"},
+            {"display": "robopitch, 窓長1024サンプル", "code": "robopitch_1024"},
+            {"display": "Inharmonic Warping, all, 一次関数", "code": "iw_all_1"},
+            {"display": "Inharmonic Warping, vib, 一次関数", "code": "iw_vib_1"},
+            {"display": "Inharmonic Warping, all, 二次関数", "code": "iw_all_2"},
+            {"display": "Inharmonic Warping, vib, 二次関数", "code": "iw_vib_2"}
+        ]
     if request.method == "GET":
-        return render_template("index.html")
+        return render_template("index.html", methods=methods)
     if request.method == "POST":
-        method = request.form["process_method"]
+        select_method = request.form["process_method"]
+        for i in range(len(methods)):
+            if methods[i]["code"] == select_method:
+                methods[i]["selected"] = True
         result = int(request.form["parameter"])
         infile = "/work/miyamoto/bthesis/wav_original/CJF04/01.wav"
-        if method == "robopitch_512":
+        if select_method == "robopitch_512":
             threshold = result * 3 / 100
             filepath = robopitch(infile, 512, threshold, "blackman")
-        elif method == "robopitch_1024":
+        elif select_method == "robopitch_1024":
             threshold = result * 3 / 100
             filepath = robopitch(infile, 1024, threshold, "blackman")
-        elif method == "iw_all_1":
+        elif select_method == "iw_all_1":
             threshold = result * 120 / 100
             filepath = inharmonic_warping(infile, "all", 1, threshold)
-        elif method == "iw_all_2":
+        elif select_method == "iw_all_2":
             threshold = result * 64 / 100
             filepath = inharmonic_warping(infile, "all", 2, threshold)
-        elif method == "iw_vib_1":
+        elif select_method == "iw_vib_1":
             threshold = result * 120 / 100
             filepath = inharmonic_warping(infile, "vib", 1, threshold)
-        elif method == "iw_vib_2":
+        elif select_method == "iw_vib_2":
             threshold = result * 64 / 100
             filepath = inharmonic_warping(infile, "vib", 2, threshold)
         
-        return render_template("index.html", filepath=filepath, result=result)
+        return render_template("index.html", methods=methods, filepath=filepath, result=result)
 
 if __name__ == "__main__":
     app.run(port=8000, debug=True)
