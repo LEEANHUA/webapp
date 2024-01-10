8bfb404171ef7b3805ce8093d4c9ae3fa6297d56
Ren Miyamoto <miyamoto@gavo.t.u-tokyo.ac.jp>
2024-01-10 15:19:44 +0900

    flask runしたときに外部からアクセスできるように変更

--- apps/app.py
+++ apps/app.py
@@ -2,10 +2,33 @@ from flask import Flask
 from apps.config import config
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
+from werkzeug.routing import Rule
+import os
 
 db = SQLAlchemy()
 
 app = Flask(__name__)
+
+# 全てのURLにprefixを追加するための処理
+APP_ROOT = os.getenv("APP_ROOT")
+if not APP_ROOT is None:
+    # define custom_rule class
+    class Custom_Rule(Rule):
+        def __init__(self, string, *args, **kwargs):
+            # check endswith '/'
+            if APP_ROOT.endswith('/'):
+                prefix_without_end_slash = APP_ROOT.rstrip('/')
+            else:
+                prefix_without_end_slash = APP_ROOT
+            # check startswith '/'
+            if APP_ROOT.startswith('/'):
+                prefix = prefix_without_end_slash
+            else:
+                prefix = '/' + prefix_without_end_slash
+            super(Custom_Rule, self).__init__(prefix + string, *args, **kwargs)
+
+    # set url_rule_class
+    app.url_rule_class = Custom_Rule
 
 app.config.from_object(config["testing"])
 
@@ -20,5 +43,6 @@ from apps.processor import views as processor_views
 
 app.register_blueprint(processor_views.processor, url_prefix="/processor")
 
+# 外部からのアクセスを許可するためには、debug=Falseとし、hostを設定する必要がある
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=False, host='0.0.0.0')
