02955215a12a659c7e6385ca6c79327599725bcc
Ren Miyamoto <miyamoto@gavo.t.u-tokyo.ac.jp>
2024-02-02 15:09:31 +0900

    実験の方式に合わせて画像データセットと音声を変更する

--- apps/guide/views.py
+++ apps/guide/views.py
@@ -4,25 +4,83 @@ import uuid
 
 guide = Blueprint("guide", __name__, template_folder="templates")
 
-@guide.route("/")
-def index():
-    robot_names = ["man", "woman", "cyborg_man", "cyborg_woman", "cyborg", "robot_with_al_1", "robot_with_al_2", "robot_without_al_1", "robot_without_al_2", "thing"]
-    speakers = ["CJF04", "CJF101", "EJF01", "EJF101", "EJF102", "CJM01", "EJM07", "EJM08", "EJM11", "EJM101"]
-    male_speakers = ["CJM01", "EJM07", "EJM08", "EJM11", "EJM101"]
-    female_speakers = ["CJF04", "CJF101", "EJF01", "EJF101", "EJF102"]
+robot_names = ["man", "woman", "cyborg_man", "cyborg_woman", "cyborg", "robot_with_al_1", "robot_with_al_2", "robot_without_al_1", "robot_without_al_2", "thing"]
+male_speaker = "EJM11"
+female_speaker = "CJF04"
+speakers = [male_speaker, female_speaker]
+total = 10
+
+@guide.route("/average_fullbody")
+def index_average_fullbody():
     target_images = []
     original_audio = []
-    total = 10
     for i in range(total):
-        robot_name = robot_names[random.randint(0, 9)]
+        robot_name = robot_names[random.randint(0, len(robot_names)-1)]
+        image = "robot_image/" + robot_name + "/" + robot_name + "_" + str(random.randint(1, 10)) + ".png"
+        target_images.append(image)
+        audio_number = format(random.randint(1, 50), '02')
+        audio = "average_audio/" + audio_number + ".wav"
+        original_audio.append(audio)
+    session["target_images"] = target_images
+    session["original_audio"] = original_audio
+    session["total"] = total
+    session["uuid"] = str(uuid.uuid4())
+    return render_template("guide/index.html")
+
+@guide.route("/average_face")
+def index_average_face():
+    target_images = []
+    original_audio = []
+    for i in range(total):
+        robot_name = robot_names[random.randint(0, len(robot_names)-1)]
+        image = "robot_face_image/" + robot_name + "/" + robot_name + "_" + str(random.randint(1, 10)) + ".png"
+        target_images.append(image)
+        audio_number = format(random.randint(1, 50), '02')
+        audio = "average_audio/" + audio_number + ".wav"
+        original_audio.append(audio)
+    session["target_images"] = target_images
+    session["original_audio"] = original_audio
+    session["total"] = total
+    session["uuid"] = str(uuid.uuid4())
+    return render_template("guide/index.html")
+
+@guide.route("/real_fullbody")
+def index_real_fullbody():
+    target_images = []
+    original_audio = []
+    for i in range(total):
+        robot_name = robot_names[random.randint(0, len(robot_names)-1)]
         image = "robot_image/" + robot_name + "/" + robot_name + "_" + str(random.randint(1, 10)) + ".png"
         target_images.append(image)
         if robot_name == "man" or robot_name == "cyborg_man":
-            speaker = male_speakers[random.randint(0, 4)]
+            speaker = male_speaker
         elif robot_name == "woman" or robot_name == "cyborg_woman":
-            speaker = female_speakers[random.randint(0, 4)]
+            speaker = female_speaker
         else:
-            speaker = speakers[random.randint(0, 9)]
+            speaker = speakers[random.randint(0, len(speakers)-1)]
+        audio_number = format(random.randint(1, 50), '02')
+        audio = "wav_original/" + speaker + "/" + audio_number + ".wav"
+        original_audio.append(audio)
+    session["target_images"] = target_images
+    session["original_audio"] = original_audio
+    session["total"] = total
+    session["uuid"] = str(uuid.uuid4())
+    return render_template("guide/index.html")
+
+@guide.route("/real_face")
+def index_real_face():
+    target_images = []
+    original_audio = []
+    for i in range(total):
+        robot_name = robot_names[random.randint(0, len(robot_names)-1)]
+        image = "robot_face_image/" + robot_name + "/" + robot_name + "_" + str(random.randint(1, 10)) + ".png"
+        target_images.append(image)
+        if robot_name == "man" or robot_name == "cyborg_man":
+            speaker = male_speaker
+        elif robot_name == "woman" or robot_name == "cyborg_woman":
+            speaker = female_speaker
+        else:
+            speaker = speakers[random.randint(0, len(speakers)-1)]
         audio_number = format(random.randint(1, 50), '02')
         audio = "wav_original/" + speaker + "/" + audio_number + ".wav"
         original_audio.append(audio)
