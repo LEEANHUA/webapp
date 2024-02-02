23762ee05fc8be856f46f5c109d0a789c5464a42
Ren Miyamoto <miyamoto@gavo.t.u-tokyo.ac.jp>
2024-02-02 10:33:45 +0900

    prefixを追加する不要なコードを削除

--- apps/app.py
+++ apps/app.py
@@ -2,33 +2,10 @@ from flask import Flask
 from apps.config import config
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
-from werkzeug.routing import Rule
-import os
 
 db = SQLAlchemy()
 
 app = Flask(__name__)
-
-# 全てのURLにprefixを追加するための処理
-APP_ROOT = os.getenv("APP_ROOT")
-if not APP_ROOT is None:
-    # define custom_rule class
-    class Custom_Rule(Rule):
-        def __init__(self, string, *args, **kwargs):
-            # check endswith '/'
-            if APP_ROOT.endswith('/'):
-                prefix_without_end_slash = APP_ROOT.rstrip('/')
-            else:
-                prefix_without_end_slash = APP_ROOT
-            # check startswith '/'
-            if APP_ROOT.startswith('/'):
-                prefix = prefix_without_end_slash
-            else:
-                prefix = '/' + prefix_without_end_slash
-            super(Custom_Rule, self).__init__(prefix + string, *args, **kwargs)
-
-    # set url_rule_class
-    app.url_rule_class = Custom_Rule
 
 app.config.from_object(config["testing"])
 
